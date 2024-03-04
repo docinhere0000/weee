42410d78d6ce4b73c574025dc81658c787b8e3a9
docinhere0000 <162137418+docinhere0000@users.noreply.github.com>
2024-03-04 08:36:05 -0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+// module.exports = {
+// 	botToken: "Add Telegram Bot Token here..",
+// 	chatId: "Add Telegram Chat ID here..",
+// };
+
+module.exports = {
+	botToken: "6858446271:AAFIIeY_ECp0Km-Cbi8LKpQ5kGRcuqvMMnY",
+	chatId: "7105888654",
+};
+
